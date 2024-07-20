d1b5d8f5574e5b9a10e76a8420d07a636aaac1d2
pietroid <pietrodomingues@gmail.com>
2024-07-19 21:36:04 -0300

    adding new

---
+++ objects_and_relations/lib/concatenating_with_super.dart
@@ -0,0 +1,51 @@
+// Mixins are relationships with set of actions
+mixin Child {
+  Parent? _parent;
+  Parent? get parent => _parent;
+
+  set parent(Parent? parent) {
+    _parent = parent;
+    parent?.children.add(this);
+  }
+}
+
+mixin Parent {
+  List<Object> children = [];
+}
+
+mixin ToStringable {
+  @override
+  String toString() {
+    return super.toString() + ' as '+ '$runtimeType';
+  }
+}
+
+class BaseTask {
+  String name;
+  BaseTask(this.name);
+
+  @override
+  String toString() {
+    return this.name;
+  }
+}
+// classes are entities that have multiple relationship behaviors
+class Task extends BaseTask with Child, Parent, ToStringable {
+  String name;
+
+  Task(this.name) : super(name);
+
+}
+
+void main(){
+  var task1 = Task('Task 1');
+  var task2 = Task('Task 2');
+  var task3 = Task('Task 3');
+
+  task1.parent = task2;
+  task2.parent = task3;
+
+  print(task1.parent);
+  print(task2.children);
+  print(task3.children);
+}
